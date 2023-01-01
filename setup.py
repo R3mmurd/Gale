2a1db3fbb32b9401b9ab3240b8819943c6a5d7b4
Alejandro <aledrums@gmail.com>
2022-12-31 20:13:42 -0500

    Some experiment

--- setup.py
+++ setup.py
@@ -1,25 +1,7 @@
-import os, setuptools
+import setuptools
+import site
+import sys
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="Gale",
-    version="0.0.2",
-    author="Alejandro Mujica",
-    author_email="aledrums@gmail.com",
-    description="Collection of reusable codes to make games by using pygame",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/R3mmurd/gale",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
-    install_requires=["wheel", "pygame", "click"]
-)
+setuptools.setup()
